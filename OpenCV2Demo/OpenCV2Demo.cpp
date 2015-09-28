4e53e50b3c83f49b9283ae3a07859adfd477c4a3
Jeremie <lucheng989898@126.com>
2015-09-28 18:27:40 +0800

    Step 1 ok

--- OpenCV2Demo/OpenCV2Demo.cpp
+++ OpenCV2Demo/OpenCV2Demo.cpp
@@ -1,11 +1,51 @@
-// OpenCV2Demo.cpp : 定义控制台应用程序的入口点。
-//
 
 #include "stdafx.h"
 
 
+#include <opencv2\opencv.hpp>
+using namespace cv;
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
 int main()
 {
+	string filepath;
+	std::cout << "输入文件：" << std::endl;
+	std::cin >> filepath;
+	cv::Mat image = cv::imread(filepath);
+	if (image.empty())
+	{
+		std::cout << "文件读取失败。" << std::endl;
+		return -1;
+	}
+	cv::imshow("原始图像", image);
+	cv::waitKey();
+	if ( 3 != image.channels() )
+	{
+		std::cout << "图像是黑白的。" << std::endl;
+	}
+	else
+	{
+		std::cout << "图像是彩色的。" << std::endl;
+		cv::cvtColor(image, image, CV_BGR2GRAY);
+		cv::imshow("灰度化的黑白图像", image);
+		cv::waitKey();
+	}
+	cv::adaptiveThreshold(image, image, 150, THRESH_BINARY, 1, 5, 2);
+	cv::imshow("自适应二值化的黑白图像", image);
+	cv::waitKey();
+
+	image = 255 - image;
+	cv::imshow("翻转的二值化图像", image);
+	cv::waitKey();
+
+
+
+
+
     return 0;
 }
 
--- OpenCV2Demo/stdafx.h
+++ OpenCV2Demo/stdafx.h
@@ -7,9 +7,6 @@
 
 #include "targetver.h"
 
-#include <stdio.h>
-#include <tchar.h>
-
 
 
 // TODO:  在此处引用程序需要的其他头文件
